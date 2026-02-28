c6aa59ef52c442fc015d6bb5f45f4e1bd68da8b4
ccb982 <2264184962@qq.com>
2026-02-28 18:15:25 +0800

    feat: your message here

--- verify_clear.sql
+++
@@ -1,4 +0,0 @@
-USE galgame_db;
-SELECT 'Games count:' as info, COUNT(*) as count FROM game;
-SELECT 'Scenes count:' as info, COUNT(*) as count FROM scene;
-SELECT 'Saves count:' as info, COUNT(*) as count FROM save;
